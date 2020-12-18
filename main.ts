c49f8b7d35d81c2d56c1b0fbf45a190741c3baec
MRRobledo <69201062+MRRobledo@users.noreply.github.com>
2020-12-18 07:36:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,31 +9,31 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false)
 })
-let level = 0
 let tree: Sprite = null
 let speed = 0
 let mySprite: Sprite = null
 game.showLongText("Usa la flecha arriba o botton A para evitar los obstaculos", DialogLayout.Bottom)
-scene.setBackgroundColor(0)
+scene.setBackgroundColor(9)
 let Obstacles = [sprites.duck.log6, sprites.duck.log7, sprites.duck.log4, sprites.duck.log8]
 mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . 5 . . 5 . 5 5 . 5 . . 5 . . 
-    . . 5 5 . 5 . 5 5 . 5 . 5 5 . . 
-    . . . 5 7 7 7 7 7 7 7 7 5 . . . 
-    . . . 7 7 7 7 7 7 7 7 7 7 . . . 
-    . . 7 7 f f 7 7 7 7 f f 7 7 . . 
-    . . 7 7 f f 7 7 7 7 f f 7 7 . . 
-    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
-    . . 7 7 7 7 7 f f 7 7 7 7 7 . . 
-    . . 7 7 7 7 f f f f 7 7 7 7 . . 
-    . . 7 7 7 7 7 f f 7 7 7 7 7 . . 
-    . . . 7 7 7 7 7 7 7 7 7 7 . . . 
-    . . . . 7 7 7 7 7 7 7 7 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    ........................................
+    ........................................
+    ........22...........f...f..............
+    ........dd...........ff.ff..............
+    ........dd............f.f...............
+    .......2222...........444...............
+    .fffff.22ffffffffffff.4442..............
+    .f2222fff22....4444444444...............
+    .f222222ffffff..44444444................
+    .f222222222222f..4444444................
+    .f222222222222f..4.4.4.4................
+    .ffffffffffffff..4.4.4.4................
+    5f............5..4...4..................
+    .55555555555555.........................
+    ........................................
+    ........................................
     `, SpriteKind.Player)
+mySprite.setPosition(60, 64)
 speed = 10
 let count = 0
 let floor = sprites.create(img`
@@ -46,7 +46,12 @@ floor = sprites.create(img`
     55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
     `, SpriteKind.Enemy)
 floor.setPosition(76, 1)
-game.onUpdateInterval(2000 / 1, function () {
+game.onUpdateInterval(2000, function () {
+    tree = sprites.create(Obstacles[randint(0, 3)], SpriteKind.Enemy)
+    tree.setPosition(170, 0)
+    tree.setVelocity(-50, 0)
+})
+game.onUpdateInterval(2000, function () {
     count += 1
     if (count >= 10) {
         count = randint(1, 9)
@@ -59,18 +64,10 @@ game.onUpdateInterval(2000 / 1, function () {
         tree.setVelocity(-50, 0)
     }
 })
-game.onUpdateInterval(2000 / 1, function () {
-    tree = sprites.create(Obstacles[randint(0, 3)], SpriteKind.Enemy)
-    tree.setPosition(170, 0)
-    tree.setVelocity(-50, 0)
-})
 forever(function () {
     mySprite.setVelocity(0, speed)
     pause(200)
     speed += 20
-})
-game.onUpdateInterval(30000, function () {
-    level += 1
 })
 game.onUpdateInterval(500, function () {
     info.changeScoreBy(10)
